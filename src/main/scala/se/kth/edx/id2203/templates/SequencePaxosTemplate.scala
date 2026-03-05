4702e9eaee7e809822df6d6f859e55c0c4e6face
Mar-i-cel <mnavarros@edu.tecnocampus.cat>
2026-03-05 22:34:14 +0000

    assigment 4 done

--- src/main/scala/se/kth/edx/id2203/templates/SequencePaxosTemplate.scala
+++ src/main/scala/se/kth/edx/id2203/templates/SequencePaxosTemplate.scala
@@ -1,21 +1,22 @@
+/// {"gradingToken":[45,45,45,45,45,66,69,71,73,78,32,80,71,80,32,77,69,83,83,65,71,69,45,45,45,45,45,10,86,101,114,115,105,111,110,58,32,66,67,80,71,32,118,49,46,53,53,10,10,104,81,73,77,65,57,79,116,77,118,84,69,98,74,49,102,65,81,47,47,82,120,70,106,72,47,122,102,85,100,100,70,75,51,107,112,120,111,78,105,50,50,50,110,103,117,87,66,115,85,87,81,115,105,107,86,73,101,101,65,69,111,103,83,10,78,121,120,84,47,119,103,121,71,54,78,54,100,117,111,102,106,102,83,108,76,109,101,66,52,48,71,71,107,106,48,50,80,68,88,101,87,53,51,76,84,120,66,77,74,119,115,54,121,86,83,107,80,68,108,81,57,74,52,49,117,82,103,90,10,103,115,50,78,111,106,104,120,82,99,56,116,49,51,106,87,84,85,73,78,103,105,55,113,121,78,52,70,54,107,66,71,101,48,43,116,112,109,120,114,98,101,48,88,113,99,81,90,118,104,73,122,49,104,116,69,110,69,107,122,110,83,65,68,10,43,86,76,53,48,86,79,86,47,105,55,50,79,119,97,72,83,90,117,53,110,117,50,66,83,113,81,105,74,72,69,56,75,49,83,66,115,102,53,83,43,117,69,74,77,86,48,87,114,100,122,65,107,79,117,109,105,69,120,68,122,66,57,54,10,81,106,75,121,83,104,77,80,111,117,43,53,48,112,81,47,113,77,70,98,83,67,81,54,103,89,98,98,103,107,51,77,103,78,70,103,102,87,57,106,67,78,82,87,100,56,51,104,87,114,97,102,55,105,115,115,106,56,54,112,106,48,89,47,10,71,119,65,99,119,108,97,56,48,55,105,99,102,81,78,112,48,97,69,51,57,49,119,66,56,84,121,74,83,110,72,106,53,103,70,115,103,57,89,109,81,119,68,43,120,119,55,67,119,109,65,102,103,55,81,98,72,79,51,121,109,119,65,48,10,47,53,53,110,102,118,80,85,83,101,88,113,116,116,105,108,99,114,82,77,55,118,118,70,69,71,105,53,84,122,76,76,54,67,70,55,77,117,115,87,86,72,104,51,105,116,68,82,66,109,65,54,82,54,113,57,53,54,84,103,112,49,82,47,10,78,80,117,106,74,103,48,81,71,98,57,98,76,113,111,114,120,109,101,70,107,83,103,72,120,87,75,88,53,55,118,100,77,117,43,56,78,52,47,90,120,77,57,56,68,106,114,84,118,66,72,121,69,69,66,90,115,72,77,117,112,50,56,72,10,50,117,43,71,111,100,78,107,82,111,119,54,122,105,67,81,55,122,49,98,97,99,88,78,47,97,51,122,57,117,117,72,117,109,65,102,100,97,79,66,114,77,52,56,80,82,68,57,43,104,72,116,80,79,100,67,101,106,106,47,106,52,43,110,10,109,74,104,43,103,79,81,51,99,74,117,71,51,113,113,116,68,104,57,110,55,100,90,43,90,87,49,48,100,90,82,71,122,72,67,52,43,50,49,50,88,86,66,72,120,69,105,119,119,81,110,111,82,100,110,52,103,98,116,69,106,99,76,81,10,51,75,52,118,86,55,72,117,119,99,47,68,103,99,68,70,118,43,72,120,68,53,109,68,119,54,90,81,72,47,74,121,103,71,108,47,102,102,88,50,51,122,117,117,83,65,50,86,103,55,102,85,105,102,109,77,106,54,108,113,120,67,118,83,10,119,68,69,66,108,114,86,86,107,103,80,117,85,86,122,67,49,80,77,118,43,88,103,73,113,71,103,101,102,80,70,70,72,80,57,89,108,79,82,80,65,110,102,50,100,100,113,83,88,113,68,65,66,76,88,77,71,112,51,69,84,118,77,119,10,65,121,111,78,89,82,48,108,97,122,70,78,69,88,48,56,88,72,77,97,47,108,76,111,82,51,107,52,48,106,120,116,101,84,52,77,69,54,114,68,83,87,75,65,118,103,70,101,105,121,66,87,114,54,48,104,43,90,55,99,85,53,122,65,10,72,52,109,77,78,108,53,80,81,65,115,105,100,74,117,47,72,119,76,101,104,53,99,90,118,85,110,65,113,72,47,80,43,54,68,101,48,48,74,66,68,120,101,68,87,121,76,68,73,101,71,69,112,122,105,82,87,116,100,83,118,55,114,109,10,109,103,85,76,122,98,121,89,87,49,81,74,121,85,51,72,76,81,109,69,53,52,50,76,69,82,81,103,71,51,102,109,115,82,106,82,120,83,52,81,57,108,121,114,121,49,99,74,83,120,120,76,66,117,89,114,88,83,97,70,54,102,111,99,10,87,122,105,110,69,104,111,109,79,54,50,117,66,48,65,118,90,67,112,111,55,84,70,113,100,53,115,48,98,104,117,113,120,71,43,98,47,85,47,122,47,43,69,101,119,109,114,99,75,66,120,65,99,49,98,72,73,118,53,56,88,54,109,109,10,52,76,52,113,10,61,120,108,121,47,10,45,45,45,45,45,69,78,68,32,80,71,80,32,77,69,83,83,65,71,69,45,45,45,45,45,10]}
 /// In this programming assignment you will have to complete the implementation of a Leader-Based Sequence Consensus.
 
 /// Sequence Consensus abstraction, in Kompics terms, is a composite component that **provides** the following port *(already imported)*.
 
-/// class SequenceConsensus extends Port 
-/// { 
+/// class SequenceConsensus extends Port
+/// {
 ///  request[SC_Propose];
 ///  indication[SC_Decide];
 /// }
-/// In your implementation, the **Sequence Consensus** component should indicate with a `SC_Decide` message every value that is appended in the decided sequence (invoked in FIFO order) 
+/// In your implementation, the **Sequence Consensus** component should indicate with a `SC_Decide` message every value that is appended in the decided sequence (invoked in FIFO order)
 /// as well as requesting proposed values through a `SC_Propose` message:
 /// case class SC_Propose(value: RSM_Command) extends KompicsEvent;
 /// case class SC_Decide(value: RSM_Command) extends KompicsEvent;
 
-/// Your sequence consensus component builds on top of a `Ballot Leader Election` component (simulated, no need to provide an implementation). 
-/// That means that it subscribes to `BLE_Leader(Leader, Ballot)` indications whenever a new leader is elected alongside a maximum ballot. 
-/// Furthermore, a `FIFOPerfectLink` abstraction is also provided to send and receive messages in FIFO order between every two processes, 
-/// using the same messages as the PerfectLink port introduced in Part I (i.e., using `PL_Send(destination, payload)` and `PL_Receive(sender, payload)` messages)  
+/// Your sequence consensus component builds on top of a `Ballot Leader Election` component (simulated, no need to provide an implementation).
+/// That means that it subscribes to `BLE_Leader(Leader, Ballot)` indications whenever a new leader is elected alongside a maximum ballot.
+/// Furthermore, a `FIFOPerfectLink` abstraction is also provided to send and receive messages in FIFO order between every two processes,
+/// using the same messages as the PerfectLink port introduced in Part I (i.e., using `PL_Send(destination, payload)` and `PL_Receive(sender, payload)` messages)
 /// The following properties define the expected behavior of a consensus abstraction more specifically:
 /// 1. **Validity**: *Every value decided in a sequence has been previously proposed*
 /// 2. **Uniform Agreement**: *For every two sequences decided across different processs, one is a prefix of the other.*
@@ -23,8 +24,6 @@
 
 /// The recommended algorithm to use is the the one we call \"Leader-Based Sequence Paxos\" which extends and optimises single value paxos to work with sequences of values.
 /// You can find the algorithm in the following link https://courses.edx.org/asset-v1:KTHx+ID2203.2x+2016T4+type@asset+block@sequence-paxos.pdf
-
-
 
 package se.kth.edx.id2203.templates
 
@@ -39,8 +38,10 @@ import collection.mutable
 
 object SequencePaxos {
   case class Prepare(nL: Long, ld: Int, na: Long) extends KompicsEvent;
-  case class Promise(nL: Long, na: Long, suffix: List[RSM_Command], ld: Int) extends KompicsEvent;
-  case class AcceptSync(nL: Long, suffix: List[RSM_Command], ld: Int) extends KompicsEvent;
+  case class Promise(nL: Long, na: Long, suffix: List[RSM_Command], ld: Int)
+      extends KompicsEvent;
+  case class AcceptSync(nL: Long, suffix: List[RSM_Command], ld: Int)
+      extends KompicsEvent;
   case class Accept(nL: Long, c: RSM_Command) extends KompicsEvent;
   case class Accepted(nL: Long, m: Int) extends KompicsEvent;
   case class Decide(ld: Int, nL: Long) extends KompicsEvent;
@@ -66,15 +67,16 @@ class SequencePaxos(init: Init[SequencePaxos]) extends ComponentDefinition {
   val pl = requires[FIFOPerfectLink];
 
   val (self, pi, others) = init match {
-    case Init(addr: Address, pi: Set[Address] @unchecked) => (addr, pi, pi - addr)
+    case Init(addr: Address, pi: Set[Address] @unchecked) =>
+      (addr, pi, pi - addr)
   }
   val majority = (pi.size / 2) + 1;
 
   var state = (FOLLOWER, UNKOWN);
-  var nL = 0l;
-  var nProm = 0l;
+  var nL = 0L;
+  var nProm = 0L;
   var leader: Option[Address] = None;
-  var na = 0l;
+  var na = 0L;
   var va = List.empty[RSM_Command];
   var ld = 0;
   // leader state
@@ -94,110 +96,107 @@ class SequencePaxos(init: Init[SequencePaxos]) extends ComponentDefinition {
 
   ble uponEvent {
     case BLE_Leader(l, n) => {
-        /* INSERT YOUR CODE HERE */
-        if(n < nL){
-          leader = l;
-          nL= n;
-          if(self == l && nL > nProm){
-            state= (LEADER, PREPARE);
-            propCmds = List.empty[RSM_Command];
-            las = 0;
-            lds = List.empty[RSM_Command];
-            acks = 0;
-            lc = 0;
-            for (p <- topology\self) {
-               trigger(FIFOPerfectLink_Send(p, Prepare(nL,ld, na)) -> pl);
-            }
-            acks[l] = (na,suffix(va,ld));
-            lds[self]=ld;
-            nProm= nL;
-          }else{
-            state= (FOLLOWER, state.2);
+      /* INSERT YOUR CODE HERE */
+      if (n > nL) {
+        leader = Some(l);
+        nL = n;
+        if (self == l && nL > nProm) {
+          state = (LEADER, PREPARE);
+          propCmds = List.empty[RSM_Command];
+          las.clear();
+          lds.clear();
+          acks.clear();
+          lc = 0;
+
+          for (p <- others) {
+            trigger(PL_Send(p, Prepare(nL, ld, na)) -> pl);
           }
+
+          acks(self) = (na, suffix(va, ld));
+          lds(self) = ld;
+          nProm = nL;
+        } else {
+          state = (FOLLOWER, state._2);
         }
+      }
     }
   }
 
   pl uponEvent {
     case PL_Deliver(p, Prepare(np, ldp, n)) => {
-        /* INSERT YOUR CODE HERE */
-        if(nProm < np){
-          nProm = np;
-          state= (FOLLOWER, PREPARE);
-          if(na >= n){
-            sfx = suffix(va, ld);
-          }
-          else{
-            sfx = List.empty[RSM_Command];
-          }
-          trigger(FIFOPerfectLink_Send(p, Promise(np,na, sfx, ld)) -> pl);
-        }
+      /* INSERT YOUR CODE HERE */
+      if (nProm < np) {
+        nProm = np;
+        state = (FOLLOWER, PREPARE);
+
+        val sfx = if (na >= n) suffix(va, ldp) else List.empty[RSM_Command];
+        trigger(PL_Send(p, Promise(np, na, sfx, ld)) -> pl);
+      }
     }
     case PL_Deliver(a, Promise(n, na, sfxa, lda)) => {
       if ((n == nL) && (state == (LEADER, PREPARE))) {
         /* INSERT YOUR CODE HERE */
-        acks[a]= (na, sfxa);
-        lds[a]= (lda);
-        //P := {p ∈ Π | acks[p] 6= ⊥}
-        if (p== majority){
-          //(k, sfx ) := max{acks[p] | p ∈ P}
-          va = prefix(va, ld) + sfx + propCmds;
-          las[self ] = |va|;
+        acks(a) = (na, sfxa);
+        lds(a) = lda;
+
+        val promisedCount = acks.size;
+        if (promisedCount == majority) {
+          val (_, sfx) = acks.values.maxBy(_._1);
+          va = prefix(va, ld) ++ sfx ++ propCmds;
+          las(self) = va.size;
           propCmds = List.empty[RSM_Command];
           state = (LEADER, ACCEPT);
-          for all p ∈ {p ∈ Π | lds[p] 6= ⊥ ∧ p 6= self } {
-            sfxp = suffix(va, lds[p]);
-            trigger(FIFOPerfectLink_Send(p, AcceptSync( nL, sfxp, lds[pd])) -> pl);
+
+          for ((p, pld) <- lds if p != self) {
+            val sfxp = suffix(va, pld);
+            trigger(PL_Send(p, AcceptSync(nL, sfxp, pld)) -> pl);
           }
         }
 
       } else if ((n == nL) && (state == (LEADER, ACCEPT))) {
         /* INSERT YOUR CODE HERE */
-        lds[a] = lda;
-        sfx = suffix(va, lds[a]);
-        trigger(FIFOPerfectLink_Send(a, AcceptSync( nL, sfx, lds[a])) -> pl);
-        if(lc != 0){
-          trigger(FIFOPerfectLink_Send(a, Decide(ld, nL)) -> pl);
+        lds(a) = lda;
+        val sfx = suffix(va, lds(a));
+        trigger(PL_Send(a, AcceptSync(nL, sfx, lds(a))) -> pl);
+        if (lc != 0) {
+          trigger(PL_Send(a, Decide(ld, nL)) -> pl);
         }
       }
     }
     case PL_Deliver(p, AcceptSync(nL, sfx, ldp)) => {
       if ((nProm == nL) && (state == (FOLLOWER, PREPARE))) {
         /* INSERT YOUR CODE HERE */
-        if(nProm == nL){
-          na = nL;
-          va = prefix(va, ld) + sfxv;
-          trigger(FIFOPerfectLink_Send(p, Accepted(nL, |va|)) -> pl);
-          state = (FOLLOWER, ACCEPT)
-        }
+        na = nL;
+        va = prefix(va, ldp) ++ sfx;
+        trigger(PL_Send(p, Accepted(nL, va.size)) -> pl);
+        state = (FOLLOWER, ACCEPT);
       }
     }
     case PL_Deliver(p, Accept(nL, c)) => {
       if ((nProm == nL) && (state == (FOLLOWER, ACCEPT))) {
         /* INSERT YOUR CODE HERE */
-        if (nprom == nL) {
-          va = va + c;
-          trigger(FIFOPerfectLink_Send(p, Accepted(nL, |va|)) -> pl);
-        }
+        va = va :+ c;
+        trigger(PL_Send(p, Accepted(nL, va.size)) -> pl);
       }
     }
     case PL_Deliver(_, Decide(l, nL)) => {
-        /* INSERT YOUR CODE HERE */
-        if (nprom == nL){
-          while (ld < l){
-            trigger(_, Decide(va[ld]) -> pl);
-            ld = ld + 1;
-          }
+      /* INSERT YOUR CODE HERE */
+      if (nProm == nL) {
+        while (ld < l) {
+          trigger(SC_Decide(va(ld)) -> sc);
+          ld += 1;
         }
+      }
     }
     case PL_Deliver(a, Accepted(n, m)) => {
       if ((n == nL) && (state == (LEADER, ACCEPT))) {
         /* INSERT YOUR CODE HERE */
-        las[a] = m;
-        if lc < m && |{p ∈ Π | las[p] ≥ m}| ≥ majority {
+        las(a) = m;
+        if (lc < m && las.values.count(_ >= m) >= majority) {
           lc = m;
-          for all p ∈ {p ∈ Π | lds[p] 6= ⊥} {
-            trigger(FIFOPerfectLink_Send(a, Decide( lc, nL)) -> pl);
+
+          for (p <- lds.keys) {
+            trigger(PL_Send(p, Decide(lc, nL)) -> pl);
           }
         }
       }
@@ -208,21 +207,19 @@ class SequencePaxos(init: Init[SequencePaxos]) extends ComponentDefinition {
     case SC_Propose(c) => {
       if (state == (LEADER, PREPARE)) {
         /* INSERT YOUR CODE HERE */
-        propCmds = propCmds + c;
-      } 
-      else if (state == (LEADER, ACCEPT)) {
+        propCmds = propCmds :+ c;
+      } else if (state == (LEADER, ACCEPT)) {
         /* INSERT YOUR CODE HERE */
-        va = va + c;
-        las[self ] = las[self ] + 1
-        for all p ∈ {p ∈ Π | lds[p] 6= ⊥ ∧ p 6= self } {
-          trigger(FIFOPerfectLink_Send(p, Accept( nL, c)) -> pl);
+        va = va :+ c;
+        las(self) = las(self) + 1;
+
+        for ((p, _) <- lds if p != self) {
+          trigger(PL_Send(p, Accept(nL, c)) -> pl);
         }
       }
     }
   }
 }
-
-
 
 object SeqConsensus extends App {
   checkSeqConsensus[SequencePaxos]();
